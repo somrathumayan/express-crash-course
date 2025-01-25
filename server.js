03d4eb1c75d48bd3ab7fb50d6bb77c05d044b9ee
somrathumayan <somrathumayan@gmail.com>
2025-01-25 22:28:31 +0600

    read file - html file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 app.get('/about', (req, res) => {
-    res.send("About");
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
 })
 app.listen(8000, () => console.log(`Server is running`));
