3d179e42f5311a120b02b8707ea7687b31a4e937
somrathumayan <somrathumayan@gmail.com>
2025-01-25 22:34:43 +0600

    create static folder

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const path = require("path");
 
 const app = express();
 
+// setup static folder 
+app.use(express.static(path.join(__dirname, 'public')))
+
+/*
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
-})
+})    
+    
+*/
+
 app.listen(8000, () => console.log(`Server is running`));
