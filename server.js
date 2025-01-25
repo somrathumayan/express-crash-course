8607b3ac53fda7c65bf47f4230ce3cd3e429436a
somrathumayan <somrathumayan@gmail.com>
2025-01-25 22:21:43 +0600

    added /about

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
-
+app.get('/about', (req, res) => {
+    res.send("About");
+})
 app.listen(8000, () => console.log(`Server is running`));
